18b40bfa5a24ecc087cc46630af4a3d4d63b64e7
chenhy05 <37240127+chenhy05@users.noreply.github.com>
2019-04-03 08:59:12 +0800

    Update config.py

--- CFCloudServer/config.py
+++ CFCloudServer/config.py
@@ -2,10 +2,11 @@
 port = '6060'
 one_day_in_seconds = 24 * 60 * 60
 
+
 # s3 config
 s3_config = { 
-    'access_key': 'AKIAJ2BO463KRGKMFD7Q', 
-    'secret_key': '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', 
+    'access_key': '', 
+    'secret_key': '', 
     'region': 'ap-southeast-1', 
     'bucket': '19cloud2' 
     }
